bcbe25ffb9fc0962070737af2c78a5b41afbb749
fatrat1117 <fatrat1117@gmail.com>
2017-12-12 20:48:20 +0800

    poc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BLE } from '@ionic-native/ble';
+import { Http, HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +37,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    BLE,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MediaCapture
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,88 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
+import { Http, RequestOptions, URLSearchParams, Headers } from '@angular/http';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  sub;
+  devices = [];
+  connectedDevice;
+  messagMap = {};
+  timeout = 1000;
+  constructor(public navCtrl: NavController,
+    private ble: BLE,
+    private http: Http,
+    private mc: MediaCapture) {
 
   }
 
+  startScan() {
+    if (this.sub)
+      return;
+    console.log('startScan');
+
+    //let self = this;
+    this.devices.splice(0);
+    this.sub = this.ble.startScan([]).subscribe(device => {
+      // setTimeout(function () {
+      //   self.devices.push(device);
+      // }, 1000);
+      this.devices.push(device);
+      console.log(device.name, this.devices);
+    })
+  }
+
+  stopScan() {
+    this.ble.stopScan();
+    if (this.sub)
+      this.sub.unsubscribe();
+    this.sub = null;
+  }
+
+  pair(device) {
+    this.ble.connect(device.id).subscribe(connectedDevice => {
+      console.log(connectedDevice);
+      if (connectedDevice.id === device.id) {
+        this.connectedDevice = connectedDevice;
+        alert('paired')
+      }
+    });
+  }
+  oneThread() {
+    let self = this;
+    setTimeout(() => {
+      if (Object.keys(this.messagMap).length < 3) {
+        self.http.get('https://api.github.com/zen').subscribe(result => {
+          if (result && 200 == result.status && result["_body"]) {
+
+            let message = result["_body"];
+            console.log(message);
+            self.messagMap[message] = true;
+          }
+        });
+      }
+    }, self.timeout);
+  }
+
+  callEndpoint() {
+    console.log(Object.keys(this.messagMap));
+    while (Object.keys(this.messagMap).length < 3) {
+      this.oneThread();
+      this.timeout += 1000;
+    }
+  }
+
+  startCaptureVideo() {
+    this.mc.captureVideo().then(files => {
+      console.log(files);
+    });
+  }
+
+  stopCaptureVideo() {
+  }
 }
